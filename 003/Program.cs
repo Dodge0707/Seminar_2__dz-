4685b1b0dc32d7cbd1f0f0112cb1074e9da5fb4c
«Vova <vrobskiy@gmail.com>
2022-04-18 22:57:57 +0300

    add Example 003

---
+++ 003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число соответствующее дню недели от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("День выходной!");
+}
+else if (number >= 1 && number <=5 )
+{
+    Console.WriteLine("День не выходной!!");
+}
+else 
+{
+    Console.WriteLine("Нет такого дня!");
+}
