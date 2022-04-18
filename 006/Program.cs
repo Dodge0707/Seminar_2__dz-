9d3d9155afd55a26c54b42838d6983b34c2b5fff
«Vova <vrobskiy@gmail.com>
2022-04-18 23:00:33 +0300

    add Example 006

---
+++ 006/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Square(numberA, numberB);
+
+void Square(int numberA, int numberB)
+{
+    int resultA = numberA * numberA;
+    int resultB = numberB * numberB;
+    if (numberA == resultB || numberB == resultA)
+    {
+        Console.WriteLine("Да!");
+    }
+    else 
+    {
+        Console.WriteLine("Нет");
+    }
+
+}
