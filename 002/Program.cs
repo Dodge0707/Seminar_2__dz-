cec7b6c4d877ba9bd6a91d7ac9b1198bacf7e587
«Vova <vrobskiy@gmail.com>
2022-04-18 22:57:04 +0300

    add Example 002

---
+++ 002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int b = number.Length;
+//Console.WriteLine(b);
+
+if (b < 3)
+{
+    Console.WriteLine("Вы ввели слишком маленькое число");
+}
+else 
+{
+    Console.WriteLine(number[2]);
+}
