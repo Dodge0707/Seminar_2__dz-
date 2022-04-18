3e31ea0913626aa9cefd167e8f9f62c80ed26aa4
«Vova <vrobskiy@gmail.com>
2022-04-18 22:59:33 +0300

    add Example 005

---
+++ 005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberA = number % 7;
+int numberB = number % 23;
+
+if (numberA == 0 && numberB == 0)
+{
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("Неа!");
+}
