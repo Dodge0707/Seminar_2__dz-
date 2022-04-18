3f412a4b4dfd5f0dee2a5e3117e2523cc5b078b9
«Vova <vrobskiy@gmail.com>
2022-04-18 22:55:31 +0300

    add Exaple 001

---
+++ 001/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberA = number / 10;
+//Console.WriteLine(numberA);
+int result = numberA % 10;
+Console.WriteLine(result);
